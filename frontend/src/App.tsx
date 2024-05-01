fd7db1f7fd5241977362eab0358931abc54824b1
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-01 15:18:46 +0200

    installed shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import LogoHeader from "./components/LogoHeader"
+
 function App() {
 
   return (
     <>
+    <LogoHeader></LogoHeader>
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">Make changes to your account here.</TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
     </>
   )
 }
---
+++ frontend/src/components/LogoHeader.tsx
@@ -0,0 +1,9 @@
+import reactSvg from "@/assets/react.svg"
+
+export default function LogoHeader() {
+  return (
+    <>
+      {reactSvg}
+    </>
+  )
+}
