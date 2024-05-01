4aed532971d87d392309de5f92a79e34df4f4785
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-01 18:17:41 +0200

    refactor: words in german language translated

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -5,13 +5,13 @@ export default function Login() {
         <form className="px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-              Username
+              E-Mail
             </label>
             <input className="focus:border-sky-600 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" />
           </div>
           <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
+              Passwort
             </label>
             <input className="focus:border-sky-600 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" />
           </div>
@@ -20,7 +20,7 @@ export default function Login() {
               Anmelden
             </button>
             <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-              Password vergessen?
+              Passwort vergessen?
             </a>
           </div>
         </form>
