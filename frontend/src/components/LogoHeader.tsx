4bd029fbfb67df145ae1fdf269aa155fe1b77193
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-01 19:30:40 +0200

    add theme toggle

--- frontend/src/components/LogoHeader.tsx
+++ frontend/src/components/LogoHeader.tsx
@@ -1,9 +1,11 @@
 import reactSvg from "@/assets/react.svg"
+import { ModeToggle } from "./mode-toggle"
 
 export default function LogoHeader() {
   return (
-    <div className="flex justify-end my-5 me-2">
+    <div className="flex justify-between my-5 me-2">
       <img src={reactSvg} />
+      <ModeToggle />
     </div>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import router from './routes'
+import { ThemeProvider } from './components/theme-provider'
 import './index.css'
+import router from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 )
