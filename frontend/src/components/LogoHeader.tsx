4c48c1c6d17ab3da46a719fe53d246910ea55e1b
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-01 18:34:27 +0200

    placed reacto logo on the right side

--- frontend/src/components/LogoHeader.tsx
+++ frontend/src/components/LogoHeader.tsx
@@ -2,8 +2,8 @@ import reactSvg from "@/assets/react.svg"
 
 export default function LogoHeader() {
   return (
-    <>
+    <div className="flex justify-end my-5 me-2">
       <img src={reactSvg} />
-    </>
+    </div>
   )
 }
