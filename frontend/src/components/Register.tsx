befeb16d98a854236f52630452f7944fdc0bb5ba
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-02 07:59:55 +0200

    changed the font color in authTabs

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -3,19 +3,19 @@ export default function Register() {
     <div className="w-full max-w-xs mx-auto">
       <form className="px-8 pt-6 pb-8 mb-4">
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+          <label className="block text-sm font-bold mb-2" htmlFor="username">
             E-Mail-Adresse
           </label>
           <input className="focus:border-sky-600 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+          <label className="block text-sm font-bold mb-2" htmlFor="password">
             Passwort
           </label>
           <input className="focus:border-sky-600 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" />
         </div>
         <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+          <label className="block text-sm font-bold mb-2" htmlFor="password">
             Passwort wiederholen
           </label>
           <input className="focus:border-sky-600 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" />
