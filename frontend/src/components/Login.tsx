91a4c62df0bbc0d853d41fe5376ce3e111925f52
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-03 08:18:11 +0200

    changed the order of hooks

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate()
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const navigate = useNavigate()
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
