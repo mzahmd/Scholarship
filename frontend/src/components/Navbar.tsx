4f190de331e233c5ccf54092e349e27edcffae48
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-03 09:16:41 +0200

    initial hidden navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Button } from "./ui/button";
 
 export default function Navbar() {
-  const [isHidden, setHidden] = useState(false)
+  const [isHidden, setHidden] = useState(true)
 
   function handleClick() {
     setHidden(prevHidden => !prevHidden)
@@ -14,13 +14,14 @@ export default function Navbar() {
     <nav className="border-gray-400 bg-gray-300 dark:bg-gray-800 dark:border-gray-700">
       <div className="flex flex-wrap justify-between p-4">
         <button onClick={handleClick} data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+          <svg className="w-5 h-5" aria-hidden="true" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
           </svg>
         </button>
         <div className="md:hidden">
-          <Button className="h-max me-2">Abmelden</Button>
+          <Link to={"/"}>
+            <Button className="h-max me-2">Abmelden</Button>
+          </ Link>
           <ModeToggle />
         </div>
         <div className={`${isHidden ? "hidden" : "block"} w-full md:block md:w-auto`} id="navbar-solid-bg">
