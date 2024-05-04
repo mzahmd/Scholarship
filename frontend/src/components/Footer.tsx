8840389af21991ec897d3191270b453aaea69691
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-04 11:37:01 +0200

    resized footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
   return (
-    <p className="text-center text-2xl text-gray-500 align-bottom">&copy; Made by Minhaz with &hearts;</p>
+    <p className="text-center text-xl text-gray-500 align-bottom">&copy; Made by Minhaz with &hearts;</p>
   )
 }
