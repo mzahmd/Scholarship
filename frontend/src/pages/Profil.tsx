c75054f766dc3d9fd03537402551feb38d5688d7
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-04 12:20:25 +0200

    color changing in dark mode for the profile header

--- frontend/src/pages/Profil.tsx
+++ frontend/src/pages/Profil.tsx
@@ -9,7 +9,7 @@ interface User {
 function ProfilInput({ label, value }: { label: string, value: string }) {
   return (
     <div className="mb-4 grid grid-cols-1 md:grid-cols-2">
-      <label className="self-end block text-sm font-bold" htmlFor="userID">{label}</label>
+      <label className="self-end block text-base font-bold" htmlFor="userID">{label}</label>
       <div>
         <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="userID" type="text" value={value} />
       </div>
@@ -30,7 +30,7 @@ export default function Profil() {
   return (
     <div className="mx-auto max-w-3xl rounded overflow-hidden shadow-lg dark:bg-slate-900">
       <div className="px-6 py-4">
-        <div className="bg-gray-200 dark:bg-slate-700 font-bold text-xl mb-2 p-1 rounded">Profil</div>
+        <div className="bg-gray-200 dark:bg-slate-900 font-bold text-2xl mb-2 p-1 rounded">Profil</div>
         <ProfilInput label={"Benutzer ID"} value={user._id} />
         <ProfilInput label={"E-Mail"} value={user.email} />
       </div>
