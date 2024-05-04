d991787e0f82a42be34215f31cce1ca7528419e1
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-04 11:42:43 +0200

    setting up admin page

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,15 +1,20 @@
 import express from "express";
 
-import { findOneUserByID } from "../models/userModel.js";
+import { findAllUser, findOneUserByID } from "../models/userModel.js";
 
 const router = express.Router();
 
 router.get("/", async function (req, res) {
-  if(req.session.userID) {
+  if (req.session.userID) {
     const user = await findOneUserByID(req.session.userID);
     return res.status(200).send(user);
   }
   res.status(409).send("failed");
+});
+
+router.get("/all", async function (req, res) {
+  const users = await findAllUser();
+  return res.status(200).send(users);
 });
 
 export default router;
--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,7 +1,48 @@
+import apiClient from "@/services/api-client";
+import { useEffect, useState } from "react"
+
+interface User {
+  _id: string;
+  email: string;
+}
+
 export default function Admin() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    apiClient.get("/api/user/all")
+      .then(response => response.data)
+      .then(data => setUsers(data))
+      .catch(e => console.log(e))
+  }, [])
+
   return (
-    <h1 className="text-4xl">
-      Administration
-    </h1>
+    <div className="overflow-x-auto">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 border border-collapse border-slate-400">
+        <thead className="text-xl text-gray-700 bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Benutzer ID
+            </th>
+            <th scope="col" className="px-6 py-3">
+              E-Mail
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {users && users.map(user =>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {user._id}
+              </th>
+              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {user.email}
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+
   )
 }
