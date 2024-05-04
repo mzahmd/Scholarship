ecfa5e1485ef35e91b50d1bbc64a46e5ae99cba8
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-04 14:13:31 +0200

    add Spinner in profile page

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,13 +1,18 @@
 import { Navigate } from "react-router-dom"
 
 import useAuth from "@/hooks/useAuth"
+import Spinner from "./Spinner"
 
 interface Props {
   children: React.ReactNode
 }
 
 export default function ProtectedRoute({ children }: Props) {
-  const { error } = useAuth()
+  const { error, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   if (error) {
     return <Navigate to={"/"} replace />
--- frontend/src/hooks/useAuth.tsx
+++ frontend/src/hooks/useAuth.tsx
@@ -4,12 +4,14 @@ import apiClient from "@/services/api-client";
 
 export default function useAuth() {
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     apiClient.get("/api/auth")
+      .then(() => setLoading(false))
       .catch((e) => setError(e.response.data))
   }, [])
 
-  return { error }
+  return { error, isLoading }
 
 }
