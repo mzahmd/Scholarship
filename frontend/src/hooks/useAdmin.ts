3f2d15adc6d3acf1e7f1df8a669b16a0e9b20ee7
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-04 15:46:34 +0200

    removed log

--- frontend/src/hooks/useAdmin.ts
+++ frontend/src/hooks/useAdmin.ts
@@ -4,9 +4,7 @@ import { useState, useEffect } from "react";
 export default function useAdmin() {
   const [isAdmin, setAdmin] = useState(false);
 
-  useEffect(() => {
-    console.log("useEffect");
-    
+  useEffect(() => {    
     apiClient.get("api/admin").then((response) => setAdmin(response.data));
   }, []);
 
