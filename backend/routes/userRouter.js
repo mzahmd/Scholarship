e191039969448ad9236bb51bb28ad49b71d0745c
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-05-05 13:33:17 +0200

    changed the error msg

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -9,12 +9,12 @@ router.get("/", async function (req, res) {
     const user = await findOneUserByID(req.session.userID);
     return res.status(200).send(user);
   }
-  res.status(409).send("failed");
+  res.status(409).send("Fehler beim Laden des Profils");
 });
 
 router.get("/all", async function (req, res) {
   if(!req.session.isAdmin) {
-    return res.status(400).send("Your shouldn't be allowed to get all users");
+    return res.status(400).send("Du bist nicht erlaubt umm alle User zu bekommen");
   }
   const users = await findAllUser();
   return res.status(200).send(users);
